fb0759010b5b0839d5630a4b5f7c0481f1ed4eb7
CarrotCoding <84916659+CarrotCoding@users.noreply.github.com>
2021-07-20 12:11:30 +0200

    finalised all the different routes

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def index():
 
 @app.route('/projects/new', methods=['GET', 'POST'])
 def add_project():
+    projects = Project.query.all()
     if request.form:
         new_project = Project(
             title=request.form['title'],
@@ -21,13 +22,14 @@ def add_project():
         db.session.add(new_project)
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('projectform.html')
+    return render_template('projectform.html', projects=projects)
 
 
 @app.route('/projects/<id>')
 def projects_detail(id):
+    projects = Project.query.all()
     project = Project.query.get_or_404(id)
-    return render_template('detail.html', project=project)
+    return render_template('detail.html', project=project, projects=projects)
 
 
 def clean_date(date_str):
@@ -39,6 +41,7 @@ def clean_date(date_str):
 
 @app.route('/projects/<id>/edit')
 def edit_project(id):
+    projects = Project.query.all()
     project = Project.query.get_or_404(id)
     if request.form:
         project.title = request.form['title']
@@ -48,10 +51,28 @@ def edit_project(id):
         project.url = request.form['github']
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('updateform.html', project=project)
+    return render_template('updateform.html', project=project, projects=projects)
 
 
-#@app.route('projects/<id>/delete')
+@app.route('/projects/<id>/delete')
+def delete_project(id):
+    projects = Project.query.all()
+    project = Project.query.get_or_404(id)
+    db.session.delete(project)
+    db.session.commit()
+    return redirect(url_for('index'), project=project, projects=projects)
+
+
+@app.route('/about')
+def about():
+    projects = Project.query.all()
+    return render_template('about.html', projects=projects)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    projects = Project.query.all()
+    return render_template('404.html', msg=error), 404
 
 
 if __name__ == '__main__':
